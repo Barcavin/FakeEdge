642dea92754f72e9f5d14671f970475beeef3cf6
barcavin <kevindong1994@gmail.com>
2022-08-11 18:24:45 -0400

    main.sh

--- main.sh
+++ main.sh
@@ -6,12 +6,15 @@ fuse=$3
 if [ "$method" = "SEAL" ]
 then
     echo "Run SEAL"
+    bash seal.sh $method $data $fuse
 elif [ "$method" = "GCN" ] || [ "$method" = "SAGE" ] || [ "$method" = "GIN" ]
 then
     echo "RUN $method"
+    bash seal.sh $method $data $fuse
 elif [ "$method" = "WalkPool" ]
 then
     echo "RUN $method"
+    bash walkpooling.sh $data $fuse
 elif [ "$method" = "PLNLP" ]
 then
     echo "RUN $method"
